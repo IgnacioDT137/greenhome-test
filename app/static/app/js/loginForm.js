b794b0941d13350f6e8401487a1033231584f1f4
Ignacio Diaz <ign.diaz@duocuc.cl>
2022-06-29 17:53:17 -0400

    se agrego el html del login, su css y su js

---
+++ app/static/app/js/loginForm.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#username').on("input", function(){
+        if($('#username').val() == ""){
+            $('#username').removeClass("ok")
+            $('#username').addClass("error")
+            $("#mensaje").show()
+        } else {
+            $("#username").removeClass("error")
+            $("#username").addClass("ok")
+            $("#mensaje").hide()
+        }
+    });
+
+    $('#passwordinput').on("input", function(){
+        if($('#passwordinput').val() == ""){
+            $("#passwordinput").removeClass("ok")
+            $("#passwordinput").addClass("error")
+            $("#mensaje2").show()
+        } else {
+            $("#passwordinput").removeClass("error")
+            $("#passwordinput").addClass("ok")
+            $("#mensaje2").hide()
+        }
+    });
+    
+    $('#form1').submit(function(){
+        if($('#username').val() == ""){
+            alert('El usuario está vacío')
+            event.preventDefault()
+        } else if($('#passwordinput').val() == ""){
+            alert('La contraseña está vacía')
+            event.preventDefault()
+        } else {
+            alert('Formulario enviado')
+        }
+    })
+})
