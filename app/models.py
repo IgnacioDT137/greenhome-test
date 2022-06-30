f14bb48dcebba6fb22d8eba28eb4e14f22eecf13
Ignacio Diaz <ign.diaz@duocuc.cl>
2022-06-29 22:49:31 -0400

    se crearon los modelos de la base de datos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+    precio = models.IntegerField(null=False)
+    stock = models.IntegerField(null=False)
+
+class Usuario(models.Model):
+    username = models.CharField(primary_key=True, max_length=30)
+    email = models.EmailField(null=False)
+    pwd = models.CharField(max_length=12, null=False)
+
+class Promocion(models.Model):
+    id_promocion = models.IntegerField(primary_key=True)
+    codigo = models.CharField(max_length=8, null=False)
+    pct = models.FloatField(null=False)
+    fecha_inicio = models.DateField(null=False)
+    fecha_fin = models.DateField(null=False)
