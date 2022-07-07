ee13c676fcc996e184847c42d40fe64d4db711f9
Ignacio Diaz <ign.diaz@duocuc.cl>
2022-07-06 23:50:11 -0400

    se crearon los modelos de funcionamiento del carrito

--- app/models.py
+++ app/models.py
@@ -27,3 +27,21 @@ class Suscripcion(models.Model):
     username = models.CharField(max_length=30)
     monto_mensual = models.IntegerField(null=False)
     fecha = models.DateTimeField(null=False, default=datetime.now())
+
+class Carrito(models.Model):
+    id_carrito = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, null=False)
+    subtotal = models.IntegerField(null=False)
+
+class CarritoItem(models.Model):
+    id_carrito = models.IntegerField(null=False)
+    id_producto = models.IntegerField(null=False)
+    nombre = models.CharField(null=False, max_length=30)
+    cantidad = models.IntegerField(null = False)
+    subtotal_producto = models.IntegerField(null=False)
+
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key=True)
+    usuario = models.CharField(null=False, max_length=100)
+    fecha = models.DateField(null=False)
+    total = models.IntegerField(null=False)
